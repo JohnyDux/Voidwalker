5f4909cf6fcdcfb8d6883a21597813570cebc11f
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2025-02-08 23:04:19 +0000

    Basic Chatbox

---
+++ Assets/Scripts/NpcInteractable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NpcInteractable : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public TextMeshProUGUI nameTag;
+    public TextMeshProUGUI dialogueTextBox;
+
+    [TextArea(3, 10)]
+    public string dialogueLine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            nameTag.text = gameObject.name;
+            dialogueTextBox.text = dialogueLine;
+            dialogueBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogueBox.SetActive(false);
+        }
+    }
+}
